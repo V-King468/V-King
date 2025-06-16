41593da4882dd87a4a38392ac96c9cf91c0f40d7
V-King468 <vasiliosantonov9@gmail.com>
2025-06-16 19:34:28 +0300

    Delete JavaScript.js

--- JavaScript.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("left").addEventListener("touchstart", () => {
-  // движение наляво
-});
-document.getElementById("right").addEventListener("touchstart", () => {
-  // движение надясно
-});
-document.getElementById("jump").addEventListener("touchstart", () => {
-  // скок
-});
