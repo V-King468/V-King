888bae032ac8a802b2a38d0348bc184b2aa6f9fb
V-King468 <vasiliosantonov9@gmail.com>
2025-06-16 09:59:22 +0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+document.getElementById("left").addEventListener("touchstart", () => {
+  // движение наляво
+});
+document.getElementById("right").addEventListener("touchstart", () => {
+  // движение надясно
+});
+document.getElementById("jump").addEventListener("touchstart", () => {
+  // скок
+});
